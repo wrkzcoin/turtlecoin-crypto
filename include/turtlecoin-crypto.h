ec49e46904e44906f0351e6faa0b7dd5be1a7848
Brandon Lehmann <brandonlehmann@gmail.com>
2019-05-30 16:00:08 -0400

    Adding variable names to help when reading the header

--- include/turtlecoin-crypto.h
+++ include/turtlecoin-crypto.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2018-2019, The TurtleCoin Developers
-// 
+//
 // Please see the included LICENSE file for more information.
 
 #pragma once
@@ -30,71 +30,71 @@ namespace Core
     {
         public:
             /* Hashing Methods */
-            static std::string cn_fast_hash(const std::string);
+            static std::string cn_fast_hash(const std::string data);
 
-            static std::string cn_slow_hash_v0(const std::string);
-            static std::string cn_slow_hash_v1(const std::string);
-            static std::string cn_slow_hash_v2(const std::string);
+            static std::string cn_slow_hash_v0(const std::string data);
+            static std::string cn_slow_hash_v1(const std::string data);
+            static std::string cn_slow_hash_v2(const std::string data);
 
-            static std::string cn_lite_slow_hash_v0(const std::string);
-            static std::string cn_lite_slow_hash_v1(const std::string);
-            static std::string cn_lite_slow_hash_v2(const std::string);
+            static std::string cn_lite_slow_hash_v0(const std::string data);
+            static std::string cn_lite_slow_hash_v1(const std::string data);
+            static std::string cn_lite_slow_hash_v2(const std::string data);
 
-            static std::string cn_dark_slow_hash_v0(const std::string);
-            static std::string cn_dark_slow_hash_v1(const std::string);
-            static std::string cn_dark_slow_hash_v2(const std::string);
+            static std::string cn_dark_slow_hash_v0(const std::string data);
+            static std::string cn_dark_slow_hash_v1(const std::string data);
+            static std::string cn_dark_slow_hash_v2(const std::string data);
 
-            static std::string cn_dark_lite_slow_hash_v0(const std::string);
-            static std::string cn_dark_lite_slow_hash_v1(const std::string);
-            static std::string cn_dark_lite_slow_hash_v2(const std::string);
+            static std::string cn_dark_lite_slow_hash_v0(const std::string data);
+            static std::string cn_dark_lite_slow_hash_v1(const std::string data);
+            static std::string cn_dark_lite_slow_hash_v2(const std::string data);
 
-            static std::string cn_turtle_slow_hash_v0(const std::string);
-            static std::string cn_turtle_slow_hash_v1(const std::string);
-            static std::string cn_turtle_slow_hash_v2(const std::string);
+            static std::string cn_turtle_slow_hash_v0(const std::string data);
+            static std::string cn_turtle_slow_hash_v1(const std::string data);
+            static std::string cn_turtle_slow_hash_v2(const std::string data);
 
-            static std::string cn_turtle_lite_slow_hash_v0(const std::string);
-            static std::string cn_turtle_lite_slow_hash_v1(const std::string);
-            static std::string cn_turtle_lite_slow_hash_v2(const std::string);
+            static std::string cn_turtle_lite_slow_hash_v0(const std::string data);
+            static std::string cn_turtle_lite_slow_hash_v1(const std::string data);
+            static std::string cn_turtle_lite_slow_hash_v2(const std::string data);
 
-            static std::string cn_soft_shell_slow_hash_v0(const std::string, const uint32_t);
-            static std::string cn_soft_shell_slow_hash_v1(const std::string, const uint32_t);
-            static std::string cn_soft_shell_slow_hash_v2(const std::string, const uint32_t);
+            static std::string cn_soft_shell_slow_hash_v0(const std::string data, const uint32_t height);
+            static std::string cn_soft_shell_slow_hash_v1(const std::string data, const uint32_t height);
+            static std::string cn_soft_shell_slow_hash_v2(const std::string data, const uint32_t height);
 
-            static std::string chukwa_slow_hash(const std::string);
+            static std::string chukwa_slow_hash(const std::string data);
 
-            static std::string tree_hash(const std::vector<std::string>);
-            static std::vector<std::string> tree_branch(const std::vector<std::string>);
-            static std::string tree_hash_from_branch(const std::vector<std::string>, const size_t depth, const std::string, const std::string);
+            static std::string tree_hash(const std::vector<std::string> hashes);
+            static std::vector<std::string> tree_branch(const std::vector<std::string> hashes);
+            static std::string tree_hash_from_branch(const std::vector<std::string> branches, const size_t depth, const std::string leaf, const std::string path);
 
             /* Crypto Methods */
             static std::tuple<bool, std::vector<std::string>> generateRingSignatures(
-                const std::string,
-                const std::string,
-                const std::vector<std::string>,
-                const std::string,
-                const uint64_t
+                const std::string prefixHash,
+                const std::string keyImage,
+                const std::vector<std::string> publicKeys,
+                const std::string transactionSecretKey,
+                const uint64_t realOutputIndex
             );
             static bool checkRingSignature(
-                const std::string, 
-                const std::string, 
-                const std::vector<std::string>, 
-                const std::vector<std::string>
+                const std::string prefixHash,
+                const std::string keyImage,
+                const std::vector<std::string> publicKeys,
+                const std::vector<std::string> signatures
             );
-            static std::string generatePrivateViewKeyFromPrivateSpendKey(const std::string);
-            static std::tuple<std::string, std::string> generateViewKeysFromPrivateSpendKey(const std::string);
+            static std::string generatePrivateViewKeyFromPrivateSpendKey(const std::string secretKey);
+            static std::tuple<std::string, std::string> generateViewKeysFromPrivateSpendKey(const std::string secretKey);
             static std::tuple<std::string, std::string> generateKeys();
-            static bool checkKey(const std::string);
-            static std::tuple<bool, std::string> secretKeyToPublicKey(const std::string);
-            static std::tuple<bool, std::string> generateKeyDerivation(const std::string, const std::string);
-            static std::tuple<bool, std::string> derivePublicKey(const std::string, const size_t, const std::string);
-            static std::string deriveSecretKey(const std::string, const size_t, const std::string);
-            static std::tuple<bool, std::string> underivePublicKey(const std::string, const size_t, const std::string);
-            static std::string generateSignature(const std::string, const std::string, const std::string);
-            static bool checkSignature(const std::string, const std::string, const std::string);
-            static std::string generateKeyImage(const std::string, const std::string);
-            static std::string scalarmultKey(const std::string, const std::string);
-            static std::string hashToEllipticCurve(const std::string);
-            static std::string scReduce32(const std::string);
+            static bool checkKey(const std::string publicKey);
+            static std::tuple<bool, std::string> secretKeyToPublicKey(const std::string secretKey);
+            static std::tuple<bool, std::string> generateKeyDerivation(const std::string publicKey, const std::string secretKey);
+            static std::tuple<bool, std::string> derivePublicKey(const std::string derivation, const size_t outputIndex, const std::string publicKey);
+            static std::string deriveSecretKey(const std::string derivation, const size_t outputIndex, const std::string secretKey);
+            static std::tuple<bool, std::string> underivePublicKey(const std::string derivation, const size_t outputIndex, const std::string derivedPublicKey);
+            static std::string generateSignature(const std::string prefixHash, const std::string publicKey, const std::string secretKey);
+            static bool checkSignature(const std::string prefixHash, const std::string publicKey, const std::string signature);
+            static std::string generateKeyImage(const std::string publicKey, const std::string secretKey);
+            static std::string scalarmultKey(const std::string keyImageA, const std::string keyImageB);
+            static std::string hashToEllipticCurve(const std::string hash);
+            static std::string scReduce32(const std::string data);
             static std::string hashToScalar(const std::string hash);
     };
 }
