7f05c57a6d5f2abc05759c3730a6ab53b91014b0
Brandon Lehmann <brandonlehmann@gmail.com>
2020-05-20 20:33:04 -0400

    Add cn_fast_hash to the benchmark script

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -35,6 +35,8 @@ console.log('')
 console.log('Starting core crypto benchmarks...')
 console.log('')
 
+runTest('cn_fast_hash', () => TurtleCoinCrypto.cn_fast_hash('00000000'))
+
 runTest('secretKeyToPublicKey',
     () => TurtleCoinCrypto.secretKeyToPublicKey(
         '9e401f727e5327080b2db6266e782356738e1eb828f39191f97771a4e401e101'
